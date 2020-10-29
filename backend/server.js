abfddb4550e728f976fbc2355c89f7a4484108b3
volodymyr-ch <cherk.v8@gmail.com>
2020-10-29 14:46:18 +0200

    [ADD] added colors package for terminal logs

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 
 import connectDB from './config/db.js';
 import products from './data/products.js';
@@ -29,5 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server is running on ${process.env.NODE_ENV} port ${PORT}`)
+  console.log(
+    `Server is running on ${process.env.NODE_ENV} port ${PORT}`.yellow.bold
+  )
 );
