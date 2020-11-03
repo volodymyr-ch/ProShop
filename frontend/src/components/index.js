c1be4599e622876a6d7d596a90dd9a2d71ff534a
volodymyr-ch <cherk.v8@gmail.com>
2020-11-03 12:00:45 +0200

    [ADD] added checkout steps component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -5,3 +5,4 @@ export { default as Product } from './Product';
 export { default as Loader } from './Loader';
 export { default as Message } from './Message';
 export { default as FormContainer } from './FormContainer';
+export { default as CheckoutSteps } from './CheckoutSteps';
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { FormContainer } from '../components';
+import { FormContainer, CheckoutSteps } from '../components';
 import { saveShippingAddress } from '../actions';
 
 const ShippingScreen = ({ history }) => {
@@ -24,6 +24,7 @@ const ShippingScreen = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
